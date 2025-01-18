52c102bef4898608fc3970b74cf8b4c8ca0317f3
Rantidhanty <rantidhanty@gmail.com>
2025-01-19 00:05:51 +0700

    feat: integrate HeroBanner component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import HeroBanner from "./components/HeroBanner";
 
 function App() {
   return (
     <div id="root">
       <Navbar />
+      <HeroBanner />
     </div>
   );
 }
